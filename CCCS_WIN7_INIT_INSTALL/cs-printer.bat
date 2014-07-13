979f6aa74fb3b2ea95041869af24eb4edff3e1dd
Roan Huang <pichuang@cs.nctu.edu.tw>
2014-07-13 17:53:20 +0800

    Add printer

--- CCCS_WIN7_INIT_INSTALL/cs-printer.bat
+++ CCCS_WIN7_INIT_INSTALL/cs-printer.bat
@@ -10,3 +10,7 @@ cscript C:\Windows\System32\Printing_Admin_Scripts\en-US\prnmngr.vbs -t -p "HP L
 cscript C:\Windows\System32\Printing_Admin_Scripts\en-US\Prnport.vbs -a -r "IP_192.168.1.243" -h 192.168.1.243 -o raw -n 9100
 cscript C:\Windows\System32\Printing_Admin_Scripts\en-US\Prnmngr.vbs -a -p "HP LaserJet 4200/4300 PCL6 C" -m "HP LaserJet 4200/4300 PCL6" -r "IP_192.168.1.243"
 cscript C:\Windows\System32\Printing_Admin_Scripts\en-US\prnmngr.vbs -t -p "HP LaserJet 4200/4300 PCL6 C"
+
+cscript C:\Windows\System32\Printing_Admin_Scripts\en-US\Prnport.vbs -a -r "IP_192.168.1.240" -h 192.168.1.240 -o raw -n 9100
+cscript C:\Windows\System32\Printing_Admin_Scripts\en-US\Prnmngr.vbs -a -p "OKI C5900(PCL)" -m "OKI C5900(PCL)" -r "IP_192.168.1.240"
+cscript C:\Windows\System32\Printing_Admin_Scripts\en-US\prnmngr.vbs -t -p "OKI C5900(PCL)"
